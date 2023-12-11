8ff6f6669094d051f0969f89d115608295f983d2
Giulio Zanet <151517159+Zanet807@users.noreply.github.com>
2023-12-11 11:19:49 +0100

    Scanner metodi HTTP
    script python che permette di analizzare i metodi HTTP di un determinato indirizzo ip

---
+++ Script Python/httpMethods.py
@@ -0,0 +1,16 @@
+import requests
+
+def scan_metodi_http(ip_bersaglio):
+	metodi_http =["GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"]
+
+	for method in metodi_http:
+
+		try:
+			url=f"http://{target_ip}/"
+			risposta = requests.request(method, url, verify=False)
+			print(f"{method}: {response.status.code}")
+		except requests.exceptions.RequestException as e:
+			print(f"{method}: Errore - {e}")
+if __name__ == "__main__":
+	ip_bersaglio=input("Inserici l'indirizzo ip target: ")
+	metodi_http(ip_bersaglio)
