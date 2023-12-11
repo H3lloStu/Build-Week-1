ec07badea743817ce88a16fb91eb85a11deef449
Giulio Zanet <151517159+Zanet807@users.noreply.github.com>
2023-12-11 12:07:56 +0100

    Scanner metodi HTTP
    Script python per l'enumerazione dei metodi HTTP abilitati su un determinato target

--- Script Python/httpMethods.py
+++ Script Python/httpMethods.py
@@ -2,15 +2,15 @@ import requests
 
 def scan_metodi_http(ip_bersaglio):
 	metodi_http =["GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"]
-#il ciclo for manda una richiesta per ogni metodo e da in risultato il codice di stato
+
 	for method in metodi_http:
+
 		try:
 			url=f"http://{ip_bersaglio}/"
 			response = requests.request(method, url, verify=False)
-			print(f"{method}: {response.status_code}")
+			print(f"{method}: {response.status.code}")
 		except requests.exceptions.RequestException as e:
 			print(f"{method}: Errore - {e}")
 if __name__ == "__main__":
-#questa sezione di codice è utilizzata in caso lo script venga avviato come "main" e non come modulo di uno script esterno
 	ip_bersaglio=input("Inserici l'indirizzo ip target: ")
 	scan_metodi_http(ip_bersaglio)
