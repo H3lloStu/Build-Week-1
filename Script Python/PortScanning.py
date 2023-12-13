df80f2d62425fef6c89db5ed8cfe0baba2df773d
Giulio Zanet <151517159+Zanet807@users.noreply.github.com>
2023-12-13 12:15:25 +0100

    PortScanning.py
    Script python che effettua una scansione delle porte attive in una macchina target

--- Script Python/PortScanning.py
+++ Script Python/PortScanning.py
@@ -1,8 +1,9 @@
 import socket
 
 def scan_ports(target_host, start_port, end_port):
+    
     print(f"Scanning {target_host} per porte aperte...\n")
-
+    #iterazione delle porte per la scansione 
     for port in range(start_port, end_port + 1):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1)
